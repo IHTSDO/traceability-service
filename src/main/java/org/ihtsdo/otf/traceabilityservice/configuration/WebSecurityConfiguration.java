3efb2d2556b2117df357821106232efa605c6756
Elena Ilyukhina <eil@ihtsdo.org>
2022-11-02 18:01:03 +0000

    PIP-202 - Added Spring Security configuration

---
+++ src/main/java/org/ihtsdo/otf/traceabilityservice/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package org.ihtsdo.otf.traceabilityservice.configuration;
+
+import org.ihtsdo.sso.integration.RequestHeaderAuthenticationDecorator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+
+		http.addFilterBefore(new RequestHeaderAuthenticationDecorator(), FilterSecurityInterceptor.class);
+
+		http.authorizeRequests()
+				.antMatchers("/version",
+						"/swagger-ui.html",
+						"/swagger-resources/**",
+						"/v2/api-docs",
+						"/webjars/springfox-swagger-ui/**").permitAll()
+				.anyRequest().authenticated()
+				.and().httpBasic();
+	}
+}
