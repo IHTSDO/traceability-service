0f483b6540edde174b5c398fc7ed52ba1e555d65
Darryn McGaw <dmcg@ihtsdo.org>
2023-10-17 15:04:52 +0100

    PIP-308 Use jakarta instead of javax

--- src/main/java/org/ihtsdo/otf/traceabilityservice/configuration/elasticsearch/LongToDateConverter.java
+++ src/main/java/org/ihtsdo/otf/traceabilityservice/configuration/elasticsearch/LongToDateConverter.java
@@ -3,9 +3,6 @@ package org.ihtsdo.otf.traceabilityservice.configuration.elasticsearch;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
-
-import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @ReadingConverter
