7b3a6ef1124d0e569a296b7a6a88bf76490702e6
CoderMChu <mch@ihtsdo.org>
2023-11-02 15:17:59 +0000

    PIP-308 Update ElasticsearchConfig to use ssl for https url

--- src/main/java/org/ihtsdo/otf/traceabilityservice/configuration/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/org/ihtsdo/otf/traceabilityservice/configuration/elasticsearch/ElasticsearchConfig.java
@@ -7,6 +7,8 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.ihtsdo.otf.traceabilityservice.configuration.ApplicationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -18,12 +20,12 @@ import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.signer.Aws4Signer;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 
-import java.time.Duration;
 import java.util.Arrays;
 
 @Configuration
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 	private final ApplicationProperties applicationProperties;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public ElasticsearchConfig(ApplicationProperties applicationProperties) {
 		this.applicationProperties = applicationProperties;
@@ -31,41 +33,55 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
 	@Override
 	public ClientConfiguration clientConfiguration() {
-		return ClientConfiguration
-				.builder()
-				.connectedTo(applicationProperties.getElasticsearchHosts())
-				.withConnectTimeout(Duration.ofSeconds(5))
-				.withSocketTimeout(Duration.ofSeconds(3))
-				.withHeaders(() -> {
-					HttpHeaders headers = new HttpHeaders();
-					if (applicationProperties.hasElasticsearchApiKey()) {
-						headers.add(HttpHeaders.AUTHORIZATION, "ApiKey " + applicationProperties.getElasticsearchApiKey());
-					}
+		HttpHeaders apiKeyHeaders = new HttpHeaders();
+		if (applicationProperties.hasElasticsearchApiKey()) {
+			logger.info("Using API key authentication.");
+			apiKeyHeaders.add("Authorization", "ApiKey " + applicationProperties.getElasticsearchApiKey());
+		}
 
-					return headers;
-				})
-				.withClientConfigurer(
-						ElasticsearchClients.ElasticsearchRestClientConfigurationCallback.from(clientBuilder -> {
-							clientBuilder.setRequestConfigCallback(builder -> {
-								//Disable lease handling for the connection pool! See https://github.com/elastic/elasticsearch/issues/24069
-								builder.setConnectionRequestTimeout(0);
-								return builder;
-							});
+		if (useHttps(applicationProperties.getElasticsearchUrls())) {
+			return ClientConfiguration.builder()
+					.connectedTo(applicationProperties.getElasticsearchHosts())
+					.usingSsl()
+					.withDefaultHeaders(apiKeyHeaders)
+					.withClientConfigurer(configureHttpClient()).build();
+		} else {
+			return ClientConfiguration.builder()
+					.connectedTo(applicationProperties.getElasticsearchHosts())
+					.withDefaultHeaders(apiKeyHeaders)
+					.withClientConfigurer(configureHttpClient()).build();
+		}
+	}
 
-							if (applicationProperties.hasElasticsearchCredentials()) {
-								final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-								credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(applicationProperties.getElasticsearchUsername(), applicationProperties.getElasticsearchPassword()));
-								clientBuilder.setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
-							}
 
-							if (applicationProperties.isAwsRequestSigning()) {
-								clientBuilder.setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.addInterceptorLast(awsInterceptor()));
-							}
+	private boolean useHttps(String[] urls) {
+		for (String url : urls) {
+			if (url.startsWith("https://")) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-							return clientBuilder;
-						})
-				)
-				.build();
+	private ElasticsearchClients.ElasticsearchRestClientConfigurationCallback configureHttpClient() {
+		return ElasticsearchClients.ElasticsearchRestClientConfigurationCallback.from(clientBuilder -> {
+			clientBuilder.setRequestConfigCallback(builder -> {
+				builder.setConnectionRequestTimeout(0);//Disable lease handling for the connection pool! See https://github.com/elastic/elasticsearch/issues/24069
+				return builder;
+			});
+			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+			if (applicationProperties.hasElasticsearchCredentials()) {
+				credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(applicationProperties.getElasticsearchUsername(), applicationProperties.getElasticsearchPassword()));
+			}
+			clientBuilder.setHttpClientConfigCallback(httpClientBuilder -> {
+				httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+				if (applicationProperties.isAwsRequestSigning()) {
+					httpClientBuilder.addInterceptorFirst(awsInterceptor());
+				}
+				return httpClientBuilder;
+			});
+			return clientBuilder;
+		});
 	}
 
 	private HttpRequestInterceptor awsInterceptor() {
